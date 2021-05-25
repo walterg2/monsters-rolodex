94377808b84d5272a714a8df125e7a7c5a8fc6af
George Walters <walterg2@gmail.com>
2021-05-25 12:18:16 -0400

    Cleaning up the card components to remove extra lines that aren't needed

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,13 +3,11 @@ import { Card } from '../card/card.component';
 
 import './card-list.styles.css';
 
-export const CardList = props => {
-  return (
-    <div className="card-list">
-      { props.monsters.map(monster => (
-        <Card key={ monster.id } monster={ monster } />
-      )) }
+export const CardList = props => (
+  <div className="card-list">
+    { props.monsters.map(monster => (
+      <Card key={ monster.id } monster={ monster } />
+    )) }
 
-    </div>
-  )
-}
+  </div>
+)
