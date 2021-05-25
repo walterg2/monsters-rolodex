c2bcec0f816c7ff6c4bac115116466688b6488a6
George Walters <walterg2@gmail.com>
2021-05-25 13:06:53 -0400

    Extracting the Search box to its own component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
 
@@ -25,10 +26,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          type="search"
+        <SearchBox
           placeholder="Search Monsters"
-          onChange={ e => this.setState({searchField: e.target.value}) } />
+          handleChange={ e => this.setState({searchField: e.target.value}) } />
         <CardList monsters={ filteredMonsters } />
     </div>
     )
